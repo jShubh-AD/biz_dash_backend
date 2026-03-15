7738bf9d54845e7110af75baeca0c9aad60f9bc0
jShubh-AD <shubhjain798@gmail.com>
2026-03-15 21:48:01 +0530

    refactor: CORS

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://bi-zeta-five.vercel.app",
-        "http://localhost:5173"
+        "http://localhost:5173",
         "http://localhost:5175"
     ],
     allow_credentials=True,
