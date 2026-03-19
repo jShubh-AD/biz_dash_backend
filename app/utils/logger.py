4e14d4c366339c3b8d421f30119de0d8f5ba319b
jShubh-AD <shubhjain798@gmail.com>
2026-03-19 17:42:11 +0530

    [REFACTOR]: enabled setup_logging to use colors

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,9 +1,18 @@
 import logging
+from colorlog import ColoredFormatter
 
 def setup_logging():
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+        formatter = ColoredFormatter(
+        "%(log_color)s%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red",
+        },)
     )
 
 def get_logger(name: str):
